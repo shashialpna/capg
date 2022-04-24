83ef0403276039180c0a5020ff1480f68d12b2bb
shashialpna <shashialpna2000@gmail.com>
2022-04-24 14:06:32 +0530

    first commit

---
+++ Day1/src/Classroom.java
@@ -0,0 +1,35 @@
+public class Student{
+	
+	String name;
+	int score;
+	
+	public Student(String name, int score) {
+		
+		this.name = name;
+		this.score = score;
+	}
+	
+	
+	
+}
+public class Classroom {
+	public String registerStudent(Student student) {
+		
+		if(ch<'A' && ch>'Z') 
+			return "Block letters needed";
+		
+		if(N<0 && N>100)
+		
+			return "invalid score";
+		else
+			return "registerd";
+		
+		
+	}
+	
+	public String studentCard(String card) {
+		
+	}
+}
+
+
---
+++ Day1/src/com/capgeminihelloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.capgeminihelloworld;
+
+public class HelloWorld {
+	//
+	
+	//
+	
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
